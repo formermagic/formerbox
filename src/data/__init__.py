8c8d33f4df53d86ac372b535f74d52b21c707967
mozharovsky <mozharovsky@live.com>
2020-07-06 11:55:35 +0300

    feat(data): init module

---
+++ src/data/__init__.py
@@ -0,0 +1,3 @@
+from .binarizer import Binarizer, dataset_dest_filepath, find_offsets
+from .indexed_dataset import IndexedDataset, IndexedDatasetBuilder
+from .max_token_batch_sampler import MaxTokensBatchSampler
