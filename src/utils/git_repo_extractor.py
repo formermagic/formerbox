70a34ef0cb103e3de75b9d4362b8be4a52b08dd0
mozharovsky <mozharovsky@live.com>
2020-07-13 18:59:35 +0300

    feat(utils): add git repo extractor for cloning repos

---
+++ src/utils/git_repo_extractor.py
@@ -0,0 +1,26 @@
+import logging
+import os
+from typing import Text
+
+from git import Repo
+
+logger = logging.getLogger(__name__)
+
+
+def convert_url_to_name(url: Text) -> Text:
+    name = os.path.basename(url)
+    name = os.path.splitext(name)[0]
+    return name
+
+
+def clone_repository(url: Text, output_path: Text) -> None:
+    logger.info("Started cloning repo with url: %s", url)
+
+    # extract basename from the given url to clone
+    basename = convert_url_to_name(url)
+    # compile a path to clone repository to
+    destpath = os.path.join(output_path, basename)
+    # clone repository to the built destination path
+    Repo.clone_from(url=url, to_path=destpath)
+
+    logging.info("Cloned %s into %s", basename, destpath)
