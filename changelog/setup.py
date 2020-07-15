723903fc3e770a84220b6d43a166b791fb961d5c
mozharovsky <mozharovsky@live.com>
2020-07-15 18:45:33 +0300

    feat(changelog): add custom cz component for changelog generation

---
+++ changelog/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+
+setup(
+    name="gitnetic-cz",
+    version="0.1.0",
+    py_modules=["cz_gitnetic"],
+    license="Not open source",
+    long_description="description for the cz",
+    install_requires=["commitizen"],
+)
