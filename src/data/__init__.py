06fbc3bcef33c97cb3b0375f83dca6bad31244c1
mozharovsky <mozharovsky@live.com>
2020-07-20 00:33:20 +0300

    fix(data): export samplers from samplers module

--- src/data/__init__.py
+++ src/data/__init__.py
@@ -1,3 +1,3 @@
 from .binarizer import Binarizer, dataset_dest_filepath, find_offsets
 from .indexed_dataset import IndexedDataset, IndexedDatasetBuilder
-from .max_token_batch_sampler import BatchSampler, MaxTokensBatchSampler
+from .samplers import BatchSampler, DistributedBatchSampler, MaxTokensBatchSampler
