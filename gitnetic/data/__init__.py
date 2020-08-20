31bfe67de70aad7ac3c5c98730d9bbe9c5fc1107
mozharovsky <mozharovsky@live.com>
2020-08-21 01:46:02 +0300

    chore(data): remove samplers from being top level symbols

--- gitnetic/data/__init__.py
+++ gitnetic/data/__init__.py
@@ -1,3 +1,2 @@
 from .binarizer import Binarizer, dataset_dest_filepath, find_offsets
 from .indexed_dataset import IndexedCachedDataset, IndexedDataset, IndexedDatasetBuilder
-from .samplers import BatchSampler, DistributedBatchSampler, MaxTokensBatchSampler
