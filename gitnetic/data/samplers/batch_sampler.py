a4326f61b44589e2c9daaa9c580b602e0f564e1f
mozharovsky <mozharovsky@live.com>
2020-08-21 01:46:55 +0300

    refactor(samplers): add batch sampler standard attributes to the base class

--- gitnetic/data/samplers/batch_sampler.py
+++ gitnetic/data/samplers/batch_sampler.py
@@ -1,17 +1,35 @@
+from __future__ import annotations
+
 from typing import Iterator, List
 
 from torch.utils.data import Dataset, Sampler
 
 
 class BatchSampler(Sampler):
-    data_source: Dataset
-
-    def __init__(self, data_source: Dataset) -> None:
+    def __init__(
+        self,
+        data_source: Dataset,
+        batch_size: int,
+        shuffle: bool = True,
+        drop_last: bool = False,
+    ) -> None:
         super().__init__(data_source)
         self.data_source = data_source
+        self.batch_size = batch_size
+        self.shuffle = shuffle
+        self.drop_last = drop_last
+
+    @classmethod
+    def from_batch_sampler(cls, batch_sampler: BatchSampler) -> BatchSampler:
+        return cls(
+            batch_sampler.data_source,
+            batch_sampler.batch_size,
+            batch_sampler.shuffle,
+            batch_sampler.drop_last,
+        )
 
     def __iter__(self) -> Iterator[List[int]]:
-        raise NotImplementedError
+        raise NotImplementedError()
 
     def __len__(self) -> int:
-        raise NotImplementedError
+        raise NotImplementedError()
