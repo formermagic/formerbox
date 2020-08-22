55081ce22fbcd07e941d82be6ad6508f45157322
mozharovsky <mozharovsky@live.com>
2020-08-22 05:59:35 +0300

    refactor(data): remove unused decorator import

--- gitnetic/data/indexed_dataset.py
+++ gitnetic/data/indexed_dataset.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 
 import os
 import struct
-from abc import abstractmethod, abstractproperty
+from abc import abstractproperty
 from functools import lru_cache
 from io import BufferedReader, BufferedWriter, FileIO
 from types import TracebackType
