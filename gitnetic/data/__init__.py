c225eee5c654a515d91f7551366f93f40500a5e9
mozharovsky <mozharovsky@live.com>
2020-08-22 09:34:42 +0300

    chore(data): export mmap dataset and builder

--- gitnetic/data/__init__.py
+++ gitnetic/data/__init__.py
@@ -1,2 +1,3 @@
 from .binarizer import Binarizer, dataset_dest_filepath, find_offsets
 from .indexed_dataset import IndexedCachedDataset, IndexedDataset, IndexedDatasetBuilder
+from .mmap_dataset import MMapIndexedDataset, MMapIndexedDatasetBuilder
