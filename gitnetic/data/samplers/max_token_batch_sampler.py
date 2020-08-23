50e586541e37f44252f11a331f573ea4b5b35c3c
mozharovsky <mozharovsky@live.com>
2020-08-23 04:14:37 +0300

    feat(samplers): add a uniform max tokens batch sampler class

--- gitnetic/data/samplers/max_token_batch_sampler.py
+++ gitnetic/data/samplers/max_token_batch_sampler.py
@@ -39,3 +39,52 @@ class UniformBatchSampler(BatchSampler):
         if self.drop_last:
             return math.floor(batch_count_float)
         return math.ceil(batch_count_float)
+
+
+class UniformMaxTokensBatchSampler(BatchSampler):
+    data_source: IndexedDatasetMixin
+
+    def __init__(
+        self,
+        data_source: IndexedDatasetMixin,
+        max_tokens: int,
+        shuffle: bool = True,
+        drop_last: bool = False,
+    ) -> None:
+        super().__init__(data_source, 0, shuffle, drop_last)
+        self.max_tokens = max_tokens
+        self._length: Optional[int] = None
+
+    def __iter__(self) -> Iterator[List[int]]:
+        sorted_indices = np.argsort(self.data_source.sizes)
+        sorted_indices = sorted_indices.tolist()
+        batches = []
+
+        batch_indices = []
+        batch_tokens = 0
+        current_index = 0
+        while current_index < len(sorted_indices):
+            index = sorted_indices[current_index]
+            batch_indices.append(index)
+            batch_tokens += self.data_source.sizes[index]
+            current_index += 1
+
+            if batch_tokens >= self.max_tokens:
+                batches.append(batch_indices)
+                batch_indices = []
+                batch_tokens = 0
+            elif not sorted_indices and not self.drop_last:
+                batches.append(batch_indices)
+
+        if self.shuffle:
+            random.shuffle(batches)
+
+        for batch in batches:
+            self.batch_size = len(batch)
+            yield batch
+
+    def __len__(self) -> int:
+        if self._length is None:
+            batches = list(iter(self))
+            self._length = len(batches)
+        return self._length
