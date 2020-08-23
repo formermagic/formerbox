34d2801e2e92dff2dc0955e2d8b2049b6e45cfcf
mozharovsky <mozharovsky@live.com>
2020-08-23 05:14:10 +0300

    feat(data): add a class to prepare indexed dataset setup from an impl choice

---
+++ gitnetic/data/indexed_dataset_setup.py
@@ -0,0 +1,69 @@
+from __future__ import annotations
+
+from argparse import ArgumentParser, Namespace
+from dataclasses import dataclass
+from typing import Any, Dict, Text, Type, Union
+
+import numpy as np
+
+from gitnetic.data import (
+    IndexedCachedDataset,
+    IndexedDataset,
+    IndexedDatasetBuilder,
+    MMapIndexedDataset,
+    MMapIndexedDatasetBuilder,
+)
+from gitnetic.data.indexed_dataset import (
+    IndexedDatasetBuilderMixin,
+    IndexedDatasetMixin,
+)
+
+
+@dataclass
+class IndexedDatasetSetup:
+    dataset_builder_type: Type[IndexedDatasetBuilderMixin]
+    dataset_type: Type[IndexedDatasetMixin]
+    dataset_dtype: np.dtype
+
+    @staticmethod
+    def add_arguments(parser: ArgumentParser) -> None:
+        parser.add_argument(
+            "--dataset_impl",
+            type=str,
+            default=None,
+            required=True,
+            choices=["lazy", "cached", "mmap"],
+            help="",
+        )
+
+    # pylint: disable=no-else-return
+    @staticmethod
+    def from_args(args: Union[Namespace, Dict[Text, Any], Text]) -> IndexedDatasetSetup:
+        if isinstance(args, Namespace):
+            args = vars(args)
+        elif isinstance(args, str):
+            args = {"dataset_impl": args}
+
+        assert "dataset_impl" in args, "Unable to find the `dataset_impl` argument."
+
+        dataset_impl = args["dataset_impl"]
+        if dataset_impl == "lazy":
+            return IndexedDatasetSetup(
+                dataset_builder_type=IndexedDatasetBuilder,
+                dataset_type=IndexedDataset,
+                dataset_dtype=np.dtype(np.int32),
+            )
+        elif dataset_impl == "cached":
+            return IndexedDatasetSetup(
+                dataset_builder_type=IndexedDatasetBuilder,
+                dataset_type=IndexedCachedDataset,
+                dataset_dtype=np.dtype(np.int32),
+            )
+        elif dataset_impl == "mmap":
+            return IndexedDatasetSetup(
+                dataset_builder_type=MMapIndexedDatasetBuilder,
+                dataset_type=MMapIndexedDataset,
+                dataset_dtype=np.dtype(np.int64),
+            )
+
+        raise ValueError("Unable to match the given dataset type.")
