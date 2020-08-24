1440d4c47e4b70110ce7d2b72c0abc51d006a670
mozharovsky <mozharovsky@live.com>
2020-08-24 03:04:31 +0300

    chore(utils): export all_subclasses method

--- gitnetic/utils/__init__.py
+++ gitnetic/utils/__init__.py
@@ -1,1 +1,7 @@
-from .utils import lazy_groups_of, lines_in_file, path_to_posix, perplexity
+from .utils import (
+    all_subclasses,
+    lazy_groups_of,
+    lines_in_file,
+    path_to_posix,
+    perplexity,
+)
