3c86bef3e6bc9a724bea3179212f18d36620c5d8
mozharovsky <mozharovsky@live.com>
2020-08-24 05:16:32 +0300

    refactor(base_transformers): make trainer buildable from functional components

--- gitnetic/tasks/base_transformers/base_trainer.py
+++ gitnetic/tasks/base_transformers/base_trainer.py
@@ -1,12 +1,15 @@
 from argparse import ArgumentParser
-from typing import Any, Dict, Text
+from typing import Any, Dict, Optional, Text, Union
 
 from pytorch_lightning import Trainer
-from transformers import PreTrainedTokenizerFast
+from transformers import (
+    PreTrainedModel,
+    PreTrainedTokenizer,
+    PreTrainedTokenizerBase,
+    PreTrainedTokenizerFast,
+)
 
-from gitnetic.data.indexed_dataset_setup import IndexedDatasetSetup
-
-from .base import DataParams, TrainingParams
+from .base import TrainingParams
 from .base_config import model_from_config, tokenizer_from_config
 from .base_lm import BaseLMDataModule, BaseLMTransformer
 
@@ -20,9 +23,9 @@ def parse_args() -> Dict[Text, Any]:
                         help="")
     parser.add_argument("--weight_decay", type=float, default=0.01, required=False,
                         help="")
-    parser.add_argument("--warmup_steps", type=int, default=None, required=False,
+    parser.add_argument("--warmup_steps", type=int, default=4000, required=False,
                         help="")
-    parser.add_argument("--learning_rate", type=float, default=None, required=True,
+    parser.add_argument("--learning_rate", type=float, default=5e-4, required=True,
                         help="")
     parser.add_argument("--power", type=float, default=1.0, required=False,
                         help="")
@@ -52,26 +55,28 @@ def parse_args() -> Dict[Text, Any]:
                         help="A seed to make experiments reproducible.")
     # fmt: on
 
+    # add lightning trainer args
+    parser = Trainer.add_argparse_args(parser)
+
     return vars(parser.parse_args())
 
 
-if __name__ == "__main__":
-    # parse the arguments
-    args = parse_args()
-
-    # prepare parsed arguments
+def make_tokenizer(args: Dict[Text, Any]) -> PreTrainedTokenizerBase:
     config_path: Text = args["config_path"]
     tokenizer_path: Text = args["tokenizer_path"]
+    tokenizer = tokenizer_from_config(config_path, tokenizer_path)
+    return tokenizer
+
+
+def make_datamodule(
+    tokenizer: Union[PreTrainedTokenizer, PreTrainedTokenizerFast],
+    args: Dict[Text, Any],
+) -> BaseLMDataModule:
     train_data_prefix: Text = args["train_data_prefix"]
     val_data_prefix: Text = args["val_data_prefix"]
     num_workers: int = args["num_workers"]
-
-    max_tokens = args["max_tokens"]
-    batch_size = args["batch_size"]
-
-    # build a tokenizer from a config file
-    tokenizer = tokenizer_from_config(config_path, tokenizer_path)
-    assert isinstance(tokenizer, PreTrainedTokenizerFast)
+    max_tokens: Optional[int] = args["max_tokens"]
+    batch_size: Optional[int] = args["batch_size"]
 
     datamodule = BaseLMDataModule(
         tokenizer=tokenizer,
@@ -82,7 +87,14 @@ if __name__ == "__main__":
         num_workers=num_workers,
     )
 
-    # build a model from a config file
+    return datamodule
+
+
+def make_model(
+    tokenizer: Union[PreTrainedTokenizer, PreTrainedTokenizerFast],
+    args: Dict[Text, Any],
+) -> PreTrainedModel:
+    config_path: Text = args["config_path"]
     model = model_from_config(
         config_path,
         vocab_size=tokenizer.vocab_size,
@@ -91,23 +103,53 @@ if __name__ == "__main__":
         eos_token_id=tokenizer.eos_token_id,
     )
 
+    return model
+
+
+def make_module(
+    model: PreTrainedModel,
+    tokenizer: Union[PreTrainedTokenizer, PreTrainedTokenizerFast],
+    args: Dict[Text, Any],
+) -> BaseLMTransformer:
+    weight_decay: float = args["weight_decay"]
+    warmup_steps: int = args["warmup_steps"]
+    learning_rate: float = args["learning_rate"]
+    power: float = args["power"]
+
     # prepare training params
     training_params = TrainingParams(
-        batch_size=batch_size,
-        max_tokens=max_tokens,
-        weight_decay=0.01,
-        warmup_steps=4_000,
-        learning_rate=5e-5,
-        power=1.0,
-    )
-
-    # prepare data params
-    data_params = DataParams(
-        dataset_impl, train_data_prefix, val_data_prefix, num_workers
+        weight_decay=weight_decay,
+        warmup_steps=warmup_steps,
+        learning_rate=learning_rate,
+        power=power,
     )
 
     # build a transformer lightning module model
-    base_lm = BaseLMTransformer(model, tokenizer, training_params, data_params)
+    module = BaseLMTransformer(model, tokenizer, training_params)
+
+    return module
+
+
+def train(args: Dict[Text, Any]) -> None:
+    # build a tokenizer
+    tokenizer = make_tokenizer(args)
+    assert isinstance(tokenizer, PreTrainedTokenizerFast)
+
+    # build a data module
+    datamodule = make_datamodule(tokenizer, args)
+
+    # build a model from a config file
+    model = make_model(tokenizer, args)
+
+    # build a language model module
+    transformer_module = make_module(model, tokenizer, args)
+
+    # TODO: parse and inject arguments for:
+    # 1) acceleration hardware setup (GPU, multi-gpu, distributed backend, etc + TPU)
+    # 2) checkpointing setup (number of steps until checkpoint, savedir, etc)
+    # 3) early stopping callbacks (e.g. stop on plateau)
+    # 4) deterministic mode toggle
+    # 5) loggers setup (especially, wandb)
 
     # prepare a trainer
     trainer = Trainer(
@@ -115,7 +157,17 @@ if __name__ == "__main__":
         gpus=0,
         replace_sampler_ddp=False,
         progress_bar_refresh_rate=1,
+        reload_dataloaders_every_epoch=True,
+        limit_train_batches=5,
+        limit_val_batches=5,
     )
 
-    # run train loop
-    trainer.fit(base_lm, datamodule=datamodule)
+    # run the train loop
+    trainer.fit(transformer_module, datamodule=datamodule)
+
+
+if __name__ == "__main__":
+    # parse the arguments
+    train_args = parse_args()
+    # run the training
+    train(train_args)
