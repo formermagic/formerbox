08d3286931d7e06f9da166c3e54597685f09d117
mozharovsky <mozharovsky@live.com>
2020-08-26 18:12:42 +0300

    fix(base_transformers): use updated filename to import correctly

--- gitnetic/tasks/base_transformers/base_trainer.py
+++ gitnetic/tasks/base_transformers/base_trainer.py
@@ -10,7 +10,7 @@ from transformers import PreTrainedModel, PreTrainedTokenizer, PreTrainedTokeniz
 
 from .base import TrainingParams
 from .base_config import model_from_config, tokenizer_from_config
-from .base_lm import TransformerDataModule, TransformerModule
+from .base_modules import TransformerDataModule, TransformerModule
 from .callbacks import SaveCheckpointAtStep
 
 
