3ec8e0911dc66f734df45a677cf8686f06554c4e
mozharovsky <mozharovsky@live.com>
2020-08-27 00:04:07 +0300

    chore(tasks): export base_transformer functional classes

---
+++ gitnetic/tasks/__init__.py
@@ -0,0 +1,9 @@
+from .base_transformers import (
+    SaveCheckpointAtStep,
+    TrainingParams,
+    TransformerDataModule,
+    TransformerModule,
+    TransformerTrainer,
+    model_from_config,
+    tokenizer_from_config,
+)
