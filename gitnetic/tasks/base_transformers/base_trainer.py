8820399498042d938f6c2e57110a909949c4a409
mozharovsky <mozharovsky@live.com>
2020-08-29 01:37:56 +0300

    refactor(base_transformers): make trainer setup much easier with tasks

--- gitnetic/tasks/base_transformers/base_trainer.py
+++ gitnetic/tasks/base_transformers/base_trainer.py
@@ -1,123 +1,49 @@
 import os
 from argparse import ArgumentParser, Namespace
 from dataclasses import dataclass
-from typing import Any, Dict, List, Optional, Text, Union
+from typing import Any, Dict, List, Text, Type
 
 from pytorch_lightning import Trainer, seed_everything
 from pytorch_lightning.callbacks import Callback, EarlyStopping
 from pytorch_lightning.loggers.wandb import LightningLoggerBase, WandbLogger
-from transformers import PreTrainedModel, PreTrainedTokenizer, PreTrainedTokenizerFast
 
-from .base import TrainingParams
-from .base_config import model_from_config, tokenizer_from_config
-from .base_modules import TransformerDataModule, TransformerModule
+from .base_task import TransformerTask
 from .callbacks import SaveCheckpointAtStep
-
-Tokenizer = Union[PreTrainedTokenizer, PreTrainedTokenizerFast]
-
-
-def parse_args() -> Dict[Text, Any]:
-    parser = ArgumentParser()
-    # fmt: off
-    parser.add_argument("--config_path", type=str, default=None, required=True,
-                        help="")
-    parser.add_argument("--tokenizer_path", type=str, default=None, required=True,
-                        help="")
-    parser.add_argument("--num_last_checkpoints", type=int, default=2, required=False,
-                        help="")
-    # fmt: on
-
-    parser = TransformerTrainer.add_argparse_args(parser)
-    parser = TransformerDataModule.add_argparse_args(parser)
-    parser = TransformerModule.add_argparse_args(parser)
-
-    return vars(parser.parse_args())
-
-
-def make_tokenizer(args: Dict[Text, Any]) -> Tokenizer:
-    config_path: Text = args["config_path"]
-    tokenizer_path: Text = args["tokenizer_path"]
-    tokenizer = tokenizer_from_config(config_path, tokenizer_path)
-    assert isinstance(tokenizer, (PreTrainedTokenizer, PreTrainedTokenizerFast))
-    return tokenizer
-
-
-def make_datamodule(
-    args: Dict[Text, Any], tokenizer: Tokenizer
-) -> TransformerDataModule:
-    train_data_prefix: Text = args["train_data_prefix"]
-    val_data_prefix: Text = args["val_data_prefix"]
-    num_workers: int = args["num_workers"]
-    max_tokens: Optional[int] = args["max_tokens"]
-    batch_size: Optional[int] = args["batch_size"]
-
-    datamodule = TransformerDataModule(
-        tokenizer=tokenizer,
-        train_data_prefix=train_data_prefix,
-        val_data_prefix=val_data_prefix,
-        max_tokens=max_tokens,
-        batch_size=batch_size,
-        num_workers=num_workers,
-    )
-
-    return datamodule
-
-
-def make_model(args: Dict[Text, Any], tokenizer: Tokenizer) -> PreTrainedModel:
-    config_path: Text = args["config_path"]
-    model = model_from_config(
-        config_path,
-        vocab_size=tokenizer.vocab_size,
-        pad_token_id=tokenizer.pad_token_id,
-        bos_token_id=tokenizer.bos_token_id,
-        eos_token_id=tokenizer.eos_token_id,
-    )
-
-    return model
-
-
-def make_module(
-    args: Dict[Text, Any],
-    model: PreTrainedModel,
-    tokenizer: Tokenizer,
-) -> TransformerModule:
-    weight_decay: float = args["weight_decay"]
-    warmup_steps: int = args["warmup_steps"]
-    learning_rate: float = args["learning_rate"]
-    power: float = args["power"]
-
-    # prepare training params
-    training_params = TrainingParams(
-        weight_decay=weight_decay,
-        warmup_steps=warmup_steps,
-        learning_rate=learning_rate,
-        power=power,
-    )
-
-    # build a transformer lightning module model
-    module = TransformerModule(model, tokenizer, training_params)
-
-    return module
 
 
 @dataclass
 class TransformerTrainer:
-    module: TransformerModule
-    datamodule: TransformerDataModule
+    args: Dict[Text, Any]
+    task: TransformerTask
 
-    def train(self, args: Dict[Text, Any]) -> None:
+    @classmethod
+    def from_task(cls, task_cls: Type[TransformerTask]) -> "TransformerTrainer":
+        # prepare the arg parser
+        parser = ArgumentParser()
+        parser = cls.add_argparse_args(parser)
+        parser = task_cls.add_argparse_args(parser)
+
+        # parse the arguments
+        args = vars(parser.parse_args())
+        # setup a task instance with args
+        task = task_cls.setup(args)
+        return cls(args, task)
+
+    def train(self, *args: Any, **kwargs: Any) -> None:
+        del args, kwargs  # reserve for future args
+
         # mark: setup deterministic mode
-        seed = args["seed"]
-        deterministic = args.pop("deterministic", False)
+        seed = self.args["seed"]
+        deterministic = self.args.pop("deterministic", False)
         if seed is not None or deterministic:
             seed_everything(seed)
 
         # mark: setup save checkpoint callbacks
         callbacks: List[Callback] = []
-        save_dir = args["save_dir"] or os.getcwd()
-        save_step_frequency = args["save_step_frequency"]
+        save_dir = self.args["save_dir"] or os.getcwd()
+        save_step_frequency = self.args["save_step_frequency"]
         if save_step_frequency is not None:
-            num_last_checkpoints = args["num_last_checkpoints"]
+            num_last_checkpoints = self.args["num_last_checkpoints"]
             save_callback = SaveCheckpointAtStep(
                 save_step_frequency, save_dir, num_last_checkpoints
             )
@@ -127,9 +53,9 @@ class TransformerTrainer:
         # mark: setup loggers
         loggers: List[LightningLoggerBase] = []
 
-        wandb_project = args["wandb_project"]
-        wandb_name = args["wandb_name"]
-        wandb_id = args["wandb_id"]
+        wandb_project = self.args["wandb_project"]
+        wandb_name = self.args["wandb_name"]
+        wandb_id = self.args["wandb_id"]
 
         required_values = [wandb_project, wandb_name]
         if all(v for v in required_values):
@@ -139,7 +65,8 @@ class TransformerTrainer:
                 id=wandb_id,
             )
 
-            wandb_logger.watch(self.module.model, log_freq=1)
+            transformer_model = self.task.module.model
+            wandb_logger.watch(transformer_model, log_freq=1)
 
             loggers.append(wandb_logger)
 
@@ -165,11 +92,11 @@ class TransformerTrainer:
         }
 
         # prepare a trainer
-        trainer_args = Namespace(**{**args, **override_kwargs})
-        trainer = Trainer.from_argparse_args(trainer_args)
+        trainer_args = Namespace(**{**self.args, **override_kwargs})
+        pl_trainer = Trainer.from_argparse_args(trainer_args)
 
         # run the train loop
-        trainer.fit(self.module, datamodule=self.datamodule)
+        pl_trainer.fit(model=self.task.module, datamodule=self.task.datamodule)
 
     @staticmethod
     def add_argparse_args(parent_parser: ArgumentParser) -> ArgumentParser:
@@ -186,13 +113,15 @@ class TransformerTrainer:
                             help="The dir to save training checkpoints.")
         parser.add_argument("--save_step_frequency", type=int, default=None, required=False,
                             help="The interval of steps between checkpoints saving.")
+        parser.add_argument("--num_last_checkpoints", type=int, default=2, required=False,
+                            help="")
         parser.add_argument("--seed", type=int, default=17, required=False,
                         help="A seed to make experiments reproducible.")
         # fmt: on
         return parser
 
 
-def train(args: Dict[Text, Any]) -> None:
+if __name__ == "__main__":
     """Running on a GPU example command:
 
     ```
@@ -212,18 +141,5 @@ def train(args: Dict[Text, Any]) -> None:
             --max_steps 10000
     ```
     """
-
-    tokenizer = make_tokenizer(args)
-    model = make_model(args, tokenizer)
-    transformer_datamodule = make_datamodule(args, tokenizer)
-    transformer_module = make_module(args, model, tokenizer)
-
-    trainer = TransformerTrainer(transformer_module, transformer_datamodule)
-    trainer.train(args)
-
-
-if __name__ == "__main__":
-    # parse the arguments
-    train_args = parse_args()
-    # run the training
-    train(train_args)
+    trainer = TransformerTrainer.from_task(TransformerTask)
+    trainer.train()
