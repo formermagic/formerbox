cb81be909f05d9ca20c9fef006342671a9c98a70
mozharovsky <mozharovsky@live.com>
2020-09-06 04:29:55 +0300

    refactor(data): make binarizer class modular, move all impl to flat-binarizer

--- gitnetic/data/binarizer.py
+++ gitnetic/data/binarizer.py
@@ -1,11 +1,23 @@
 import os
+from abc import abstractmethod
+from argparse import ArgumentParser
 from io import TextIOWrapper
-from typing import Callable, List, Optional, Text, Tuple
+from typing import Any, Callable, List, Optional, Text, Tuple
 
 import torch
 from transformers import PreTrainedTokenizerFast
+from typing_extensions import Literal
 
+from gitnetic.common.registrable import Registrable
 from gitnetic.data.indexed_dataset_setup import IndexedDatasetSetup
+from gitnetic.utils import str2bool
+
+Truncation = Literal[
+    "only_first",
+    "only_second",
+    "longest_first",
+    "do_not_truncate",
+]
 
 
 def dataset_dest_filepath(filepath_prefix: Text, extension: Text) -> Text:
@@ -45,21 +57,21 @@ def find_offsets(filename: Text, num_chunks: int) -> Tuple[int, List[int]]:
     return size, offsets
 
 
-# TODO: make modular (i.e. replacable/selectable)
-class Binarizer:
+class Binarizer(Registrable):
     # pylint: disable=too-many-arguments
     def __init__(
-        self,
-        dataset_setup: IndexedDatasetSetup,
-        tokenizer: PreTrainedTokenizerFast,
-        max_length: Optional[int] = None,
+        self, dataset_setup: IndexedDatasetSetup, tokenizer: PreTrainedTokenizerFast
     ) -> None:
         self.dataset_setup = dataset_setup
         self.tokenizer = tokenizer
-        self.max_length = max_length
 
     def binarize_dataset(
-        self, filename: Text, output_prefix: Text, start_offset: int, end_offset: int
+        self,
+        filename: Text,
+        output_prefix: Text,
+        start_offset: int,
+        end_offset: int,
+        **kwargs: Any,
     ) -> None:
         # prepare indexed dataset builder
         data_filepath = dataset_dest_filepath(output_prefix, extension="bin")
@@ -78,50 +90,103 @@ class Binarizer:
             consumer=dataset_builder.add_tokenized_ids,
             start_offset=start_offset,
             end_offset=end_offset,
-            max_length=self.max_length,
+            **kwargs,
         )
 
         # write meta data and type info
         dataset_builder.finalize()
 
-    # pylint: disable=too-many-arguments
-    @staticmethod
+    @abstractmethod
     def binarize(
+        self,
         filename: Text,
         tokenizer: PreTrainedTokenizerFast,
         consumer: Callable[[torch.Tensor], None],
         start_offset: int = 0,
         end_offset: int = -1,
-        max_length: Optional[int] = None,
+        **kwargs: Any,
     ) -> None:
         """Binarize the given chunk of file and pass to a consumer."""
+        raise NotImplementedError()
 
-        with open(filename, mode="r", encoding="utf-8") as f:
-            f.seek(start_offset)
-            line = read_line(f)
+    @staticmethod
+    @abstractmethod
+    def add_argparse_args(parent_parser: ArgumentParser) -> ArgumentParser:
+        raise NotImplementedError()
+
+
+@Binarizer.register(name="flat-binarizer")
+class FlatBinarizer(Binarizer):
+    def binarize(
+        self,
+        filename: Text,
+        tokenizer: PreTrainedTokenizerFast,
+        consumer: Callable[[torch.Tensor], None],
+        start_offset: int = 0,
+        end_offset: int = -1,
+        truncation: Truncation = "do_not_truncate",
+        max_length: Optional[int] = None,
+        stride: int = 0,
+        return_overflowing_tokens: bool = False,
+        **kwargs: Any,
+    ) -> None:
+        # pylint: disable=arguments-differ, too-many-locals, too-many-arguments
+        del kwargs  # use only designated args
+
+        # make sure we explicitly truncate if max_length is provided
+        if max_length is not None and truncation == "do_not_truncate":
+            truncation = "longest_first"
+
+        with open(filename, mode="r", encoding="utf-8") as stream:
+            stream.seek(start_offset)
+            line = read_line(stream)
             while line:
                 # remove all special chars
                 line = line.rstrip()
 
                 # check if end_offset is reached
-                if end_offset and f.tell() > end_offset:
+                if end_offset and stream.tell() > end_offset:
                     break
-
-                # TODO: support more truncation strategies
-                # prepare tokenization params
-                if max_length is not None:
-                    kwargs = {"truncation": True, "max_length": max_length}
-                else:
-                    kwargs = {}
 
                 try:
                     # tokenize input text and feed to consumer handler
-                    encoding = tokenizer(line, return_tensors="pt", **kwargs)
-                    input_ids = encoding.input_ids.squeeze()
-                    consumer(input_ids)
+                    encoding = tokenizer(
+                        line,
+                        truncation=truncation,
+                        max_length=max_length,
+                        stride=stride,
+                        return_overflowing_tokens=return_overflowing_tokens,
+                    )
+
+                    if encoding.input_ids:
+                        if isinstance(encoding.input_ids[0], list):
+                            for input_ids in encoding.input_ids:
+                                consumer(torch.tensor(input_ids))
+                        else:
+                            consumer(torch.tensor(encoding.input_ids))
+
                 except TypeError as err:
                     # TODO: add logging
                     print(f"Unable to tokenize a text, error: {err}")
 
                 # get the next line to process
-                line = f.readline()
+                line = stream.readline()
+
+    @staticmethod
+    def add_argparse_args(parent_parser: ArgumentParser) -> ArgumentParser:
+        parser = ArgumentParser(parents=[parent_parser], add_help=False)
+        truncation_choices = list(Truncation.__args__)  # type: ignore
+
+        # fmt: off
+        parser.add_argument("--truncation", type=str, choices=truncation_choices,
+                            default="do_not_truncate", required=False,
+                            help="")
+        parser.add_argument("--max_length", type=int, default=None, required=False,
+                            help="A maximum length of text sequence to encode.")
+        parser.add_argument("--stride", type=int, default=0, required=False,
+                            help="")
+        parser.add_argument("--return_overflowing_tokens", type=str2bool, default=False,
+                            required=False, help="")
+        # fmt: on
+
+        return parser
