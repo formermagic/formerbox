3033aadafc6d46989199751d4a697cfae852b67f
mozharovsky <mozharovsky@live.com>
2020-09-23 17:03:26 +0300

    feat(data): add code dataset converter impl

--- gitnetic/data/dataset_converter.py
+++ gitnetic/data/dataset_converter.py
@@ -60,3 +60,71 @@ class DatasetConverter(ArgumentRegistrable):
         self, dataset: Union[Dataset, DatasetDict], output_path: Text
     ) -> None:
         raise NotImplementedError()
+
+
+@DatasetConverter.register("code-lm-converter", constructor="from_args")
+class CodeLMDatasetConverter(DatasetConverter, DatasetProcessingMixin):
+    @dataclass
+    class Params(DataclassBase):
+        script_path: Text = field(metadata={"help": ""})
+        data_files: List[Text] = field(metadata={"help": ""})
+        output_path: Text = field(metadata={"help": ""})
+        batched: bool = field(default=True, metadata={"help": ""})
+        batch_size: int = field(default=128, metadata={"help": ""})
+        num_proc: int = field(default=1, metadata={"help": ""})
+
+    def __init__(self, params: Params) -> None:
+        self.params = params
+
+    def convert(self, *args: Any, **kwargs: Any) -> None:
+        del args, kwargs  # use only designated args
+
+        dataset = load_dataset(
+            self.params.script_path,
+            data_files=self.params.data_files,
+            split="train",
+        )
+        dataset = self.distinct_dataset(
+            dataset,
+            column="content",
+            num_proc=self.params.num_proc,
+        )
+        dataset = dataset.map(
+            self.encode,
+            batched=self.params.batched,
+            batch_size=self.params.batch_size,
+            num_proc=self.params.num_proc,
+        )
+
+        self.save_dataset(dataset, self.params.output_path)
+
+    def encode(self, instance: Dict[Text, Any]) -> Dict[Text, Any]:
+        result: Union[Text, List[Text]]
+        content = instance["content"]
+        if isinstance(content, list):
+            result = [self.tokenize_text(text) for text in content]
+        else:
+            result = self.tokenize_text(content)
+        return {"output_data": result}
+
+    def save_dataset(
+        self, dataset: Union[Dataset, DatasetDict], output_path: Text
+    ) -> None:
+        data: List[Text] = dataset["output_data"]  # type: ignore
+        with open(output_path, mode="w") as stream:
+            for group in lazy_groups_of(data, group_size=1000):
+                stream.write("\n".join(group))
+
+    @classmethod
+    def add_argparse_args(
+        cls, parent_parser: DataclassArgumentParser
+    ) -> DataclassArgumentParser:
+        return DataclassArgumentParser(
+            dataclass_types=cls.Params,
+            parents=[parent_parser],
+            add_help=False,
+        )
+
+    def tokenize_text(self, text: Text) -> Text:
+        tokens = tokenize_python(text, keep_comments=True)
+        return " ".join(tokens)
