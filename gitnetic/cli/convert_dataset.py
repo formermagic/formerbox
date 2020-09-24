b70dd2b8bfb98b884c58370f0adf4eb26aa2c5a0
mozharovsky <mozharovsky@live.com>
2020-09-24 04:30:28 +0300

    refactor(cli): make convert_dataset a cli subcommand

--- gitnetic/cli/convert_dataset.py
+++ gitnetic/cli/convert_dataset.py
@@ -1,32 +1,58 @@
+from argparse import Namespace
 from time import time
+from typing import Any, Dict, Text, Tuple, Union
 
-from gitnetic.common.dataclass_argparse import DataclassArgumentParser
+from typeguard import typechecked
+
+from gitnetic.cli import main
+from gitnetic.cli.subcommand import Subcommand, _SubParsersAction
+from gitnetic.common.dataclass_argparse import (
+    DataclassArgumentParser,
+    DataclassBase,
+    get_parsed_attr,
+)
 from gitnetic.data.dataset_converter import DatasetConverter
 
 
-def setup_parser(parent_parser: DataclassArgumentParser) -> DataclassArgumentParser:
-    parser = DataclassArgumentParser(parents=[parent_parser], add_help=False)
-    # fmt: off
-    parser.add_argument("--converter_type", type=str, default=None, required=True,
-                        help="")
-    # fmt: on
-    return parser
+@Subcommand.register("convert_dataset")
+class ConvertDataset(Subcommand):
+    def add_subparser(
+        self, parser: _SubParsersAction
+    ) -> Tuple[DataclassArgumentParser, Dict[Text, Any]]:
+        description = (
+            """Convert a raw dataset into a prepared for processing text dataset."""
+        )
+        subparser = self._add_parser(
+            parser, name=self.name, description=description, help="Train a model."
+        )
+
+        # get the type of a converter to use
+        subparser.add_argument(
+            "--converter_type",
+            type=str,
+            help="",
+        )
+
+        # add designated dataclass argparse args
+        args = subparser.parse_known_args()[0]
+        converter_cls, _ = DatasetConverter.from_registry(args.converter_type)
+        converter_cls.add_argparse_args(subparser)
+        # prepare the parser default values
+        defaults = dict(func=convert_dataset, dataclass_types=subparser.dataclass_types)
+
+        return subparser, defaults
 
 
-def main() -> None:
-    parser = DataclassArgumentParser()
-    parser = setup_parser(parser)
-    args = vars(parser.parse_known_args()[0])
-
-    converter_cls, converter_init = DatasetConverter.from_registry(
-        args.pop("converter_type")
-    )
-
+@typechecked
+def convert_dataset(params: Tuple[Union[DataclassBase, Namespace], ...]) -> None:
     parser = converter_cls.add_argparse_args(parser)
     params = parser.parse_args_into_dataclasses()
+    converter_type = get_parsed_attr(params, "converter_type")
+    assert isinstance(converter_type, str)
+    converter_cls = DatasetConverter.from_name(converter_type)
+    converter = converter_cls(params=params[0])
 
     start = time()
-    converter = converter_init(params=params[0])
     converter.convert()
     print(f"Time elapsed: {time() - start}")
 
