ed84f99521a615d2783bd291d1063adf7901100c
mozharovsky <mozharovsky@live.com>
2020-09-24 04:29:28 +0300

    feat(cli): prepare and exec parsers on subcommands

---
+++ gitnetic/cli/__init__.py
@@ -0,0 +1,46 @@
+from typing import Optional, Text
+
+from gitnetic.cli.subcommand import Subcommand
+from gitnetic.common.dataclass_argparse import DataclassArgumentParser
+from gitnetic.common.utils import import_module_and_submodules
+
+
+def make_parser(prog: Optional[Text] = None) -> DataclassArgumentParser:
+    parser = DataclassArgumentParser(prog=prog)
+    subparsers = parser.add_subparsers(
+        title="Commands", parser_class=DataclassArgumentParser
+    )
+
+    for subcommand_name in sorted(Subcommand.list_available()):
+        subcommand_cls = Subcommand.from_name(subcommand_name)
+        subcommand = subcommand_cls()
+
+        subparser, defaults = subcommand.add_subparser(subparsers)
+        subparser.set_defaults(**defaults)
+        subparser.add_argument(
+            "--include-package",
+            type=str,
+            action="append",
+            default=[],
+            help="additional packages to include",
+        )
+
+    return parser
+
+
+def main(prog: Optional[Text] = None) -> None:
+    parser = make_parser(prog)
+    args = parser.parse_known_args()[0]
+
+    # If a subparser is triggered, it adds its work as `args.func`.
+    # So if no such attribute has been added, no subparser was triggered,
+    # so give the user some help.
+    if "func" in dir(args):
+        # Import any additional modules needed (to register custom classes).
+        for package_name in args.include_package:
+            import_module_and_submodules(package_name)
+        parser.dataclass_types = args.dataclass_types
+        params = parser.parse_args_into_dataclasses()
+        args.func(params)
+    else:
+        parser.print_help()
