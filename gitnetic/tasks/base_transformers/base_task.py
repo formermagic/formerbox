87cd0f0fe8c24bc369a3cd4c9dd01195d94e6341
mozharovsky <mozharovsky@live.com>
2020-09-24 20:22:28 +0300

    refactor(base_transformers): use static typed dataclass params for task setup

--- gitnetic/tasks/base_transformers/base_task.py
+++ gitnetic/tasks/base_transformers/base_task.py
@@ -1,25 +1,28 @@
 from abc import abstractmethod
-from argparse import ArgumentParser
-from typing import Any, Text, Type, TypeVar, Union
+from argparse import Namespace
+from dataclasses import dataclass, field
+from typing import Generic, Optional, Text, Tuple, Type, TypeVar, Union
 
-from transformers import PreTrainedTokenizer, PreTrainedTokenizerFast
+from transformers import PreTrainedTokenizerBase
 
-from gitnetic.common.registrable import Registrable
+from gitnetic.common.dataclass_argparse import DataclassArgumentParser, DataclassBase
+from gitnetic.common.registrable import ArgumentRegistrable
 
-from .base import TrainingParams
 from .base_config import model_from_config, tokenizer_from_config
 from .base_modules import TransformerDataModule, TransformerModule
 
-T = TypeVar("T", bound="TaskModule")
-Tokenizer = Union[PreTrainedTokenizer, PreTrainedTokenizerFast]
+T = TypeVar("T", bound="TaskModule")  # pylint: disable=invalid-name
+Tokenizer = PreTrainedTokenizerBase
+ModuleType = TypeVar("ModuleType", bound="TransformerModule")
+DataModuleType = TypeVar("DataModuleType", bound="TransformerDataModule")
 
 
-class TaskModule(Registrable):
+class TaskModule(Generic[ModuleType, DataModuleType], ArgumentRegistrable):
     def __init__(
         self,
         tokenizer: Tokenizer,
-        module: TransformerModule,
-        datamodule: TransformerDataModule,
+        module: ModuleType,
+        datamodule: DataModuleType,
     ) -> None:
         self.tokenizer = tokenizer
         self.module = module
@@ -27,30 +30,60 @@ class TaskModule(Registrable):
 
     @classmethod
     @abstractmethod
-    def setup(cls: Type[T], *args: Any, **kwargs: Any) -> T:
-        raise NotImplementedError()
-
-    @staticmethod
-    @abstractmethod
-    def add_argparse_args(parent_parser: ArgumentParser) -> ArgumentParser:
+    def setup(
+        cls: Type[T],
+        params: Tuple[Union[DataclassBase, Namespace], ...],
+    ) -> T:
         raise NotImplementedError()
 
 
 @TaskModule.register("transformer-task")
 class TransformerTask(TaskModule):
-    @classmethod
-    def setup(cls, *args: Any, **kwargs: Any) -> "TransformerTask":
-        del args  # use designated args
+    Module = TransformerModule
+    DataModule = TransformerDataModule
 
-        # prepare the pretrained tokenizer
-        config_path: Text = kwargs["config_path"]
-        tokenizer_path: Text = kwargs["tokenizer_path"]
-        tokenizer = tokenizer_from_config(config_path, tokenizer_path)
-        assert isinstance(tokenizer, Tokenizer.__args__)  # type: ignore
+    @dataclass
+    class Params(DataclassBase):
+        config_path: Text = field(
+            metadata={"help": "A path to the file with model and tokenizer configs."},
+        )
+        tokenizer_path: Text = field(
+            metadata={"help": "A path to the dir with saved pretrained tokenizer."},
+        )
+
+    def __init__(
+        self,
+        tokenizer: Tokenizer,
+        module: Module,
+        datamodule: DataModule,
+    ) -> None:
+        super().__init__(tokenizer, module, datamodule)
+
+    @classmethod
+    def setup(
+        cls: Type["TransformerTask"],
+        params: Tuple[Union[DataclassBase, Namespace], ...],
+    ) -> "TransformerTask":
+        task_params: Optional[cls.Params] = None
+        module_params: Optional[cls.Module.Params] = None
+        datamodule_params: Optional[cls.DataModule.Params] = None
+
+        for param in params:
+            if isinstance(param, cls.Params):
+                task_params = param
+            elif isinstance(param, cls.Module.Params):
+                module_params = param
+            elif isinstance(param, cls.DataModule.Params):
+                datamodule_params = param
+
+        assert task_params is not None
+        tokenizer = tokenizer_from_config(
+            task_params.config_path, task_params.tokenizer_path
+        )
 
         # prepare a model to train
         model = model_from_config(
-            config_path,
+            task_params.config_path,
             vocab_size=tokenizer.vocab_size,
             pad_token_id=tokenizer.pad_token_id,
             bos_token_id=tokenizer.bos_token_id,
@@ -58,26 +91,19 @@ class TransformerTask(TaskModule):
         )
 
         # prepare a transformer module
-        training_params = TrainingParams.from_args(**kwargs)
-        module = TransformerModule(model, tokenizer, training_params)
+        assert module_params is not None
+        module = cls.Module(model=model, tokenizer=tokenizer, params=module_params)
 
         # prepare a transformer datamodule
-        datamodule = TransformerDataModule.from_args(tokenizer=tokenizer, **kwargs)
+        assert datamodule_params is not None
+        datamodule = cls.DataModule(tokenizer=tokenizer, params=datamodule_params)
 
         return cls(tokenizer, module, datamodule)
 
-    @staticmethod
-    def add_argparse_args(parent_parser: ArgumentParser) -> ArgumentParser:
-        parser = ArgumentParser(parents=[parent_parser], add_help=False)
-        # fmt: off
-        parser.add_argument("--config_path", type=str, default=None, required=True,
-                            help="A path to the file with model and tokenizer configs.")
-        parser.add_argument("--tokenizer_path", type=str, default=None, required=True,
-                            help="A path to the dir with saved pretrained tokenizer.")
-        # fmt: on
-
-        # parser = TransformerTrainer.add_argparse_args(parser)
-        parser = TransformerDataModule.add_argparse_args(parser)
-        parser = TransformerModule.add_argparse_args(parser)
-
-        return parser
+    @classmethod
+    def add_argparse_args(
+        cls: Type["TransformerTask"], parser: DataclassArgumentParser
+    ) -> None:
+        parser.add_arguments(cls.Params)
+        cls.Module.add_argparse_args(parser)
+        cls.DataModule.add_argparse_args(parser)
