ae6e15ea9d3fdc5cc12d9d9c918bf324b087f23c
mozharovsky <mozharovsky@live.com>
2020-09-24 12:36:07 +0300

    feat: setup main app logging

--- gitnetic/__main__.py
+++ gitnetic/__main__.py
@@ -1,4 +1,19 @@
+import logging
+import os
+import sys
+
 from gitnetic.cli import main
+
+if os.environ.get("DEBUG"):
+    LEVEL = logging.DEBUG
+else:
+    LEVEL = logging.INFO
+
+PROJECT_DIR = os.path.dirname(os.path.abspath(os.path.join(__file__, os.pardir)))
+sys.path.insert(0, PROJECT_DIR)
+
+LOGGING_FORMAT = "%(asctime)s - %(levelname)s - %(name)s: %(message)s"
+logging.basicConfig(format=LOGGING_FORMAT, level=LEVEL)
 
 
 def run_main() -> None:
