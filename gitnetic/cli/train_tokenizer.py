c19e7b1825f06d5fbdade042ad4bb5fb106e8b5f
mozharovsky <mozharovsky@live.com>
2020-09-25 02:27:00 +0300

    refactor(cli): impl subcommand for training a tokenizer

--- gitnetic/cli/train_tokenizer.py
+++ gitnetic/cli/train_tokenizer.py
@@ -1,37 +1,81 @@
-from argparse import ArgumentParser
-from typing import Any, Dict, Text
+import logging
+import os
+from argparse import Namespace, _SubParsersAction
+from dataclasses import dataclass, field
+from typing import Any, Dict, Text, Tuple, Union
 
+from typeguard import typechecked
+
+from gitnetic.cli.subcommand import Subcommand
+from gitnetic.common.dataclass_argparse import (
+    DataclassArgumentParser,
+    DataclassBase,
+    get_params_item,
+)
 from gitnetic.tasks.base_transformers import TokenizerModule
 
-
-def parse_args(parent_parser: ArgumentParser) -> Dict[Text, Any]:
-    parser = ArgumentParser(parents=[parent_parser], add_help=False)
-    # fmt: off
-    parser.add_argument("--tokenizer_type", type=str, default=None, required=True,
-                        help="")
-    # fmt: on
-
-    args = vars(parser.parse_known_args()[0])
-    return args
+logger = logging.getLogger(__name__)
 
 
-def main() -> None:
-    parser = ArgumentParser()
+@Subcommand.register("train_tokenizer")
+class TrainTokenizer(Subcommand):
+    @dataclass
+    class Params(DataclassBase):
+        tokenizer: Text = field(
+            metadata={
+                "choices": sorted(TokenizerModule.list_available()),
+                "help": "",
+            },
+        )
 
-    # parse args to build a tokenizer trainer class
-    args = parse_args(parser)
+    def add_subparser(
+        self, parser: _SubParsersAction
+    ) -> Tuple[DataclassArgumentParser, Dict[Text, Any]]:
+        description = """Train a tokenizer on text data files."""
+        subparser = self._add_parser(
+            parser,
+            name=self.name,
+            description=description,
+            help=description,
+        )
 
-    tokenizer_type = args["tokenizer_type"]
-    tokenizer_cls, _ = TokenizerModule.from_registry(tokenizer_type)
+        # add command arguments
+        subparser.add_arguments(self.Params)
 
+        def add_dynamic_args(parser: DataclassArgumentParser) -> None:
+            # get the parsed command arguments
+            parsed_params = subparser.parse_args_into_dataclasses(
+                return_remaining_strings=True
+            )
+            params = parsed_params[0]
+            assert isinstance(params, self.Params)
+
+            # add dybamic args to the subparser
+            tokenizer_cls, _ = TokenizerModule.from_registry(params.tokenizer)
+            tokenizer_cls.add_argparse_args(subparser)
+
+            # inject dataclass_types to the parent parser
+            parser.dataclass_types = subparser.dataclass_types
+
+        defaults = dict(
+            func=train_tokenizer,
+            add_dynamic_args=add_dynamic_args,
+        )
+
+        return subparser, defaults
+
+
+@typechecked
+def train_tokenizer(params: Tuple[Union[DataclassBase, Namespace], ...]) -> None:
     # add selected tokenizer's args
     parser = tokenizer_cls.add_argparse_args(parser, stage="train")
     args = vars(parser.parse_known_args()[0])
+    os.environ["TOKENIZERS_PARALLELISM"] = "true"
 
-    tokenizer = tokenizer_cls(**args)
-    tokenizer.train_tokenizer(**args)
-    tokenizer.save_pretrained(**args)
+    cmd_params = get_params_item(params, params_type=TrainTokenizer.Params)
 
+    tokenizer_cls, tokenizer_init = TokenizerModule.from_registry(cmd_params.tokenizer)
+    tokenizer_params = get_params_item(params, params_type=tokenizer_cls.Params)
+    tokenizer = tokenizer_init(params=tokenizer_params)
 
-if __name__ == "__main__":
-    main()
+    tokenizer.train_tokenizer()
