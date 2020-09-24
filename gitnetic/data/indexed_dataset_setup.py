ed82183b52c4cad3cc3957b7be7e26246661afc3
mozharovsky <mozharovsky@live.com>
2020-09-25 02:22:43 +0300

    refactor(data): use static typed dataclass args in dataset setup

--- gitnetic/data/indexed_dataset_setup.py
+++ gitnetic/data/indexed_dataset_setup.py
@@ -1,9 +1,12 @@
-from argparse import ArgumentParser, Namespace
-from dataclasses import dataclass
+from argparse import Namespace
+from dataclasses import dataclass, field
 from typing import Any, Dict, Text, Type, Union
 
 import numpy as np
+from typing_extensions import Literal
 
+from gitnetic.common.dataclass_argparse import DataclassArgumentParser, DataclassBase
+from gitnetic.common.registrable import ArgumentRegistrable
 from gitnetic.data.indexed_dataset import (
     IndexedCachedDataset,
     IndexedDataset,
@@ -14,53 +17,63 @@ from gitnetic.data.indexed_dataset import (
 from gitnetic.data.mmap_dataset import MMapIndexedDataset, MMapIndexedDatasetBuilder
 
 
-@dataclass
-class IndexedDatasetSetup:
-    dataset_builder_type: Type[IndexedDatasetBuilderBase]
-    dataset_type: Type[IndexedDatasetBase]
-    dataset_dtype: np.dtype
-
-    @staticmethod
-    def add_arguments(parser: ArgumentParser) -> None:
-        parser.add_argument(
-            "--dataset_impl",
-            type=str,
-            default=None,
-            required=True,
-            choices=["lazy", "cached", "mmap"],
-            help="Determines the type of a dataset to build.",
+class IndexedDatasetSetup(ArgumentRegistrable):
+    # pylint: disable=arguments-differ
+    @dataclass
+    class Params(DataclassBase):
+        dataset_impl: Literal["lazy", "cached", "mmap"] = field(
+            metadata={"help": "Determines the type of a dataset to build."},
         )
 
-    # pylint: disable=no-else-return
-    @staticmethod
+    def __init__(
+        self,
+        dataset_builder_type: Type[IndexedDatasetBuilderBase],
+        dataset_type: Type[IndexedDatasetBase],
+        dataset_dtype: np.dtype,
+    ) -> None:
+        self.dataset_builder_type = dataset_builder_type
+        self.dataset_type = dataset_type
+        self.dataset_dtype = dataset_dtype
+
+    @classmethod
     def from_args(
-        args: Union[Namespace, Dict[Text, Any], Text]
+        cls, args: Union[Namespace, Dict[Text, Any], Text, Params], **kwargs: Any
     ) -> "IndexedDatasetSetup":
+        del kwargs  # use only designated args
         if isinstance(args, Namespace):
             args = vars(args)
         elif isinstance(args, str):
             args = {"dataset_impl": args}
+        elif isinstance(args, cls.Params):
+            args = {"dataset_impl": args.dataset_impl}
 
         assert "dataset_impl" in args, "Unable to find the `dataset_impl` argument."
 
+        result: IndexedDatasetSetup
         dataset_impl = args["dataset_impl"]
         if dataset_impl == "lazy":
-            return IndexedDatasetSetup(
+            result = IndexedDatasetSetup(
                 dataset_builder_type=IndexedDatasetBuilder,
                 dataset_type=IndexedDataset,
                 dataset_dtype=np.dtype(np.int32),
             )
         elif dataset_impl == "cached":
-            return IndexedDatasetSetup(
+            result = IndexedDatasetSetup(
                 dataset_builder_type=IndexedDatasetBuilder,
                 dataset_type=IndexedCachedDataset,
                 dataset_dtype=np.dtype(np.int32),
             )
         elif dataset_impl == "mmap":
-            return IndexedDatasetSetup(
+            result = IndexedDatasetSetup(
                 dataset_builder_type=MMapIndexedDatasetBuilder,
                 dataset_type=MMapIndexedDataset,
                 dataset_dtype=np.dtype(np.int64),
             )
+        else:
+            raise ValueError("Unable to match the given dataset type.")
 
-        raise ValueError("Unable to match the given dataset type.")
+        return result
+
+    @classmethod
+    def add_argparse_args(cls, parser: DataclassArgumentParser) -> None:
+        parser.add_arguments(cls.Params)
