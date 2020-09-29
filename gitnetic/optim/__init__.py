7ce654f34c92063235bee18c3bd77cc14404d901
mozharovsky <mozharovsky@live.com>
2020-09-30 01:38:28 +0300

    chore(optim): export AdamW class

--- gitnetic/optim/__init__.py
+++ gitnetic/optim/__init__.py
@@ -1,1 +1,5 @@
-from .optimization import get_polynomial_decay_with_warmup, weight_decay_params
+from gitnetic.optim.optimization import (
+    AdamW,
+    get_polynomial_decay_with_warmup,
+    weight_decay_params,
+)
