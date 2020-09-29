8e88f87d356335ad0fb00e550ae74654820af769
mozharovsky <mozharovsky@live.com>
2020-09-30 01:38:13 +0300

    feat(optim): add AdamW optimizer with correct type hints

--- gitnetic/optim/optimization.py
+++ gitnetic/optim/optimization.py
@@ -1,8 +1,12 @@
-from typing import Any, Dict, List, Text
+from typing import Any, Dict, Iterable, List, Text, Tuple, Union
 
 import torch
+import transformers
+from torch import Tensor
 from torch.optim import Optimizer
 from torch.optim.lr_scheduler import LambdaLR
+
+Params = Union[Iterable[Tensor], Iterable[Dict]]
 
 
 def get_polynomial_decay_with_warmup(
@@ -38,3 +42,24 @@ def weight_decay_params(
         {"params": no_decay, "weight_decay": 0.0},
         {"params": decay, "weight_decay": weight_decay},
     ]
+
+
+class AdamW(transformers.AdamW):
+    def __init__(
+        self,
+        params: Params,
+        lr: float = 1e-3,
+        betas: Tuple[float, float] = (0.9, 0.999),
+        eps: float = 1e-6,
+        weight_decay: float = 0.0,
+        correct_bias: bool = True,
+    ) -> None:
+        # pylint: disable=too-many-arguments
+        super().__init__(
+            params=params,  # type: ignore
+            lr=lr,
+            betas=betas,
+            eps=eps,
+            weight_decay=weight_decay,
+            correct_bias=correct_bias,
+        )
