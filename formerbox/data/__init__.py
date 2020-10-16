8e3122c9786271bc98f3d4d1cf357b795d1269e3
mozharovsky <mozharovsky@live.com>
2020-10-16 19:20:12 +0300

    chore(data): update imported names

--- formerbox/data/__init__.py
+++ formerbox/data/__init__.py
@@ -1,3 +1,4 @@
-from .binarizer import Binarizer, dataset_dest_filepath, find_offsets
+from .binarizer import Binarizer, TransformerBinarizer
+from .dataset_converter import DatasetConverter, TransformerDatasetConverter
 from .indexed_dataset import IndexedCachedDataset, IndexedDataset, IndexedDatasetBuilder
 from .mmap_dataset import MMapIndexedDataset, MMapIndexedDatasetBuilder
