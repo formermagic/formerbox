6a299b80316de4c8e43cd20e4a9fb6ce036a46d4
mozharovsky <mozharovsky@live.com>
2020-10-17 00:08:49 +0300

    chore(common): export common classes

---
+++ formerbox/common/__init__.py
@@ -0,0 +1,4 @@
+from .dataclass_argparse import DataclassArgumentParser, DataclassBase
+from .has_params import HasParams, HasParsableParams
+from .partial_initable import PartialInitable
+from .registrable import Registrable
