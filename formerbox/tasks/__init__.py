42129e123dbace9951e512aab2e6227a935bd501
mozharovsky <mozharovsky@live.com>
2020-10-27 17:04:17 +0300

    chore(tasks): export renamed byte level tokenization components

--- formerbox/tasks/__init__.py
+++ formerbox/tasks/__init__.py
@@ -2,8 +2,11 @@ import os
 
 from formerbox.tasks.task_module import TaskModule
 from formerbox.tasks.transformer_task import TransformerTask
-from formerbox.tasks.transformer_tokenization import TransformerTokenizerFast
-from formerbox.tasks.transformer_tokenizer_module import TransformerTokenizerModule
+from formerbox.tasks.transformer_tokenization import ByteLevelBPETokenizerFast
+from formerbox.tasks.transformer_tokenizer_module import (
+    ByteLevelBPETokenizerModule,
+    TransformerTokenizerModule,
+)
 from formerbox.utils.utils import str2bool
 
 EXPORT_TASKS = str2bool(os.environ.get("EXPORT_TASKS", default="true"))
