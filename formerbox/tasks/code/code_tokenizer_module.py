a28d639f0c092ecbbe0b5b5f3ccfcc0a999b91db
mozharovsky <mozharovsky@live.com>
2020-10-29 06:57:58 +0300

    refactor(code): override template methods to use code bbpe fast tokenizer

--- formerbox/tasks/code/code_tokenizer_module.py
+++ formerbox/tasks/code/code_tokenizer_module.py
@@ -1,16 +1,14 @@
 import logging
 from pathlib import Path
-from typing import Any, Text, Union
+from typing import Any, Optional, Text, Union
 
 from formerbox.modules import TokenizerModule
 from formerbox.tasks import ByteLevelBPETokenizerModule
 from formerbox.tasks.code.code_tokenization import CodeBBPETokenizerFast
 from formerbox.utils.code_tokenizer import SpecialToken
 from tokenizers import AddedToken
-from transformers import PreTrainedTokenizer, PreTrainedTokenizerFast
 
 Token = Union[Text, AddedToken]
-TransformersTokenizer = Union[PreTrainedTokenizer, PreTrainedTokenizerFast]
 
 logger = logging.getLogger(__name__)
 
@@ -28,26 +26,40 @@ class CodeBBPETokenizerModule(ByteLevelBPETokenizerModule):
 
     def configure_tokenizer(
         self, tokenizer_path: Union[Text, Path], **kwargs: Any
-    ) -> TransformersTokenizer:
+    ) -> CodeBBPETokenizerFast:
+        # prepare paths to the tokenizer files
         if isinstance(tokenizer_path, str):
             tokenizer_path = Path(tokenizer_path)
         vocab_file = str(tokenizer_path / "vocab.json")
         merges_file = str(tokenizer_path / "merges.txt")
+
+        # prepare the unified pre-trained tokenizer path
+        # tokenizers will produce this file if no legacy
+        # format is specified while saving
+        tokenizer_file: Optional[Text] = None
+        if not self.params.legacy_format:
+            tokenizer_file = str(tokenizer_path / "tokenizer.json")
+
+        # merge user-defined arguments into kwargs
+        kwargs.update(self.get_tokenizer_args(self.params))
+
+        # configure the pretrained tokenizer
         return CodeBBPETokenizerFast(
-            vocab_file=vocab_file, merges_file=merges_file, **kwargs
+            vocab_file=vocab_file,
+            merges_file=merges_file,
+            tokenizer_file=tokenizer_file,
+            **kwargs,
         )
 
     @classmethod
-    def from_pretrained(cls, params: Params, **kwargs: Any) -> TransformersTokenizer:
-        # prepare init arguments from params
-        assert params.tokenizer_path is not None
-        init_kwargs = cls.get_tokenizer_args(params)
-        kwargs.update(init_kwargs)
+    def from_pretrained(
+        cls, tokenizer_path: Union[Text, Path], **kwargs: Any
+    ) -> CodeBBPETokenizerFast:
+        # convert `tokenizer_path` to string
+        if isinstance(tokenizer_path, Path):
+            tokenizer_path = str(tokenizer_path)
 
-        # get the pretrained tokenizer
-        tokenizer = CodeBBPETokenizerFast.from_pretrained(
-            params.tokenizer_path, **kwargs
-        )
-        assert isinstance(tokenizer, PreTrainedTokenizerFast)
+        # load the pretrained tokenizer
+        tokenizer = CodeBBPETokenizerFast.from_pretrained(tokenizer_path, **kwargs)
 
         return tokenizer
