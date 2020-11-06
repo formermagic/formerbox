fa2779766127bcd6cf02f573a859171034042adf
mozharovsky <mozharovsky@live.com>
2020-11-06 15:34:42 +0300

    chore(tasks): export renamed classes and paths

--- formerbox/tasks/__init__.py
+++ formerbox/tasks/__init__.py
@@ -1,19 +1,17 @@
 import os
 
 from formerbox.tasks.task_module import TaskModule
+from formerbox.tasks.tokenization_roberta import RobertaTokenizer
+from formerbox.tasks.tokenization_roberta_trainer import RobertaTokenizerTrainer
+from formerbox.tasks.tokenization_trainer import BaseTokenizerTrainer
 from formerbox.tasks.transformer_task import TransformerTask
-from formerbox.tasks.transformer_tokenization import ByteLevelBPETokenizerFast
-from formerbox.tasks.transformer_tokenizer_module import (
-    ByteLevelBPETokenizerModule,
-    TransformerTokenizerModule,
-)
 from formerbox.utils.utils import str2bool
 
 EXPORT_TASKS = str2bool(os.environ.get("EXPORT_TASKS", default="true"))
 
 if EXPORT_TASKS:
     from formerbox.tasks.code import (
-        CodeBBPETokenizerFast,
-        CodeBBPETokenizerModule,
         CodeDatasetConverter,
+        CodeRobertaTokenizer,
+        CodeRobertaTokenizerTrainer,
     )
