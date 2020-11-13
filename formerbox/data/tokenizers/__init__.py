74126676579bb93f41e7f5c8352935a5b212f15e
mozharovsky <mozharovsky@live.com>
2020-11-14 00:26:09 +0300

    chore(tokenizers): export tokenization classes

---
+++ formerbox/data/tokenizers/__init__.py
@@ -0,0 +1,6 @@
+from formerbox.data.tokenizers.tokenization_base import Seq2SeqTokenizer, TokenizerBase
+from formerbox.data.tokenizers.tokenization_roberta import RobertaTokenizer
+from formerbox.data.tokenizers.tokenization_roberta_trainer import (
+    RobertaTokenizerTrainer,
+)
+from formerbox.data.tokenizers.tokenization_trainer import TokenizerTrainerBase
