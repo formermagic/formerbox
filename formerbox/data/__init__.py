e16273505c27060b08af07f30a4ef7168be79fdb
mozharovsky <mozharovsky@live.com>
2020-11-14 00:27:00 +0300

    chore(data): define full import paths

--- formerbox/data/__init__.py
+++ formerbox/data/__init__.py
@@ -1,4 +1,11 @@
-from .binarizer import Binarizer, TransformerBinarizer
-from .dataset_converter import DatasetConverter, TransformerDatasetConverter
-from .indexed_dataset import IndexedCachedDataset, IndexedDataset, IndexedDatasetBuilder
-from .mmap_dataset import MMapIndexedDataset, MMapIndexedDatasetBuilder
+from formerbox.data.binarizer import Binarizer, TransformerBinarizer
+from formerbox.data.dataset_converter import (
+    DatasetConverter,
+    TransformerDatasetConverter,
+)
+from formerbox.data.indexed_dataset import (
+    IndexedCachedDataset,
+    IndexedDataset,
+    IndexedDatasetBuilder,
+)
+from formerbox.data.mmap_dataset import MMapIndexedDataset, MMapIndexedDatasetBuilder
