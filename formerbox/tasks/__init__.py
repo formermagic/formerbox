15077dbd3e06083c0d9c16ef65e474ef6d605b9d
mozharovsky <mozharovsky@live.com>
2020-11-14 00:28:04 +0300

    chore(tasks): remove outdated tokenizer imports from tasks

--- formerbox/tasks/__init__.py
+++ formerbox/tasks/__init__.py
@@ -1,10 +1,6 @@
 import os
 
 from formerbox.tasks.task_module import TaskModule
-from formerbox.tasks.tokenization_base import TokenizerBase
-from formerbox.tasks.tokenization_roberta import RobertaTokenizer
-from formerbox.tasks.tokenization_roberta_trainer import RobertaTokenizerTrainer
-from formerbox.tasks.tokenization_trainer import TokenizerTrainerBase
 from formerbox.tasks.transformer_task import TransformerTask
 from formerbox.utils.utils import str2bool
 
