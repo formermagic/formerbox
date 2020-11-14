649c1ec695ede24fe3fee63ea13e4f9c4f1566ff
mozharovsky <mozharovsky@live.com>
2020-11-14 03:49:49 +0300

    chore(common): export missing magic constant

--- formerbox/common/__init__.py
+++ formerbox/common/__init__.py
@@ -1,4 +1,8 @@
-from formerbox.common.dataclass_argparse import DataclassArgumentParser, DataclassBase
+from formerbox.common.dataclass_argparse import (
+    MISSING,
+    DataclassArgumentParser,
+    DataclassBase,
+)
 from formerbox.common.has_params import HasParams, HasParsableParams
 from formerbox.common.partial_initable import PartialInitable
 from formerbox.common.registrable import Registrable
