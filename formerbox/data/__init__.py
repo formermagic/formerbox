5648ef78543050a5404ecdc671548ddee6d53dfe
mozharovsky <mozharovsky@live.com>
2020-11-14 05:53:21 +0300

    chore(data): export renamed binarizer and dataset converter classes

--- formerbox/data/__init__.py
+++ formerbox/data/__init__.py
@@ -1,8 +1,5 @@
-from formerbox.data.binarizer import Binarizer, TransformerBinarizer
-from formerbox.data.dataset_converter import (
-    DatasetConverter,
-    TransformerDatasetConverter,
-)
+from formerbox.data.binarizer import Binarizer, DefaultBinarizer
+from formerbox.data.dataset_converter import DatasetConverter, DefaultDatasetConverter
 from formerbox.data.indexed_dataset import (
     IndexedCachedDataset,
     IndexedDataset,
