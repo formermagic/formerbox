36ba3a7f58763beca844aaf4056931afa719e89a
mozharovsky <mozharovsky@live.com>
2020-11-14 05:52:56 +0300

    refactor(data): set missing default values for required user args

--- formerbox/data/dataset_converter.py
+++ formerbox/data/dataset_converter.py
@@ -6,7 +6,7 @@ from pathlib import Path
 from typing import Any, Dict, List, Optional, Text, Type, Union
 
 from datasets import Dataset, DatasetDict, load_dataset
-from formerbox.common.dataclass_argparse import DataclassBase
+from formerbox.common.dataclass_argparse import MISSING, DataclassBase
 from formerbox.common.has_params import HasParsableParams, ParamsType
 from formerbox.common.registrable import Registrable
 from formerbox.utils import append_path_suffix, lazy_groups_of
@@ -76,6 +76,7 @@ class DefaultDatasetConverter(DatasetConverter, DatasetProcessingMixin):
     @dataclass
     class Params(DataclassBase):
         script_path: Text = field(
+            default=MISSING,
             metadata={
                 "help": "The path to the dataset processing script with the dataset builder."
                 " Can be either: \n"
@@ -83,13 +84,15 @@ class DefaultDatasetConverter(DatasetConverter, DatasetProcessingMixin):
                 " (if the script has the same name as the directory)\n"
                 " * a dataset identifier on HuggingFace AWS bucket (list all available"
                 " datasets and ids with datasets.list_datasets())."
-            }
+            },
         )
         output_path: Text = field(
-            metadata={"help": "The output directory to save converted text datasets."}
+            default=MISSING,
+            metadata={"help": "The output directory to save converted text datasets."},
         )
         data_files: List[Text] = field(
-            metadata={"help": "Defining the data_files of the dataset configuration"}
+            default_factory=MISSING,
+            metadata={"help": "Defining the data_files of the dataset configuration"},
         )
         train_test_split: bool = field(
             default=False,
