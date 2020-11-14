3e28ad6bda070e22a8e02e2ae10988157d400d57
mozharovsky <mozharovsky@live.com>
2020-11-14 04:37:37 +0300

    chore(tokenizers): export tokenizer trainer params from tokenizers module

--- formerbox/data/tokenizers/__init__.py
+++ formerbox/data/tokenizers/__init__.py
@@ -7,4 +7,7 @@ from formerbox.data.tokenizers.tokenization_roberta import RobertaTokenizer
 from formerbox.data.tokenizers.tokenization_roberta_trainer import (
     RobertaTokenizerTrainer,
 )
-from formerbox.data.tokenizers.tokenization_trainer import TokenizerTrainerBase
+from formerbox.data.tokenizers.tokenization_trainer import (
+    TokenizerTrainerBase,
+    TokenizerTrainerParams,
+)
