40e3dfcfc87b2673a476ba1418673ffffa45d6ab
mozharovsky <mozharovsky@live.com>
2020-11-30 10:11:35 +0300

    chore(data): export DatasetIterator class from data module

--- formerbox/data/__init__.py
+++ formerbox/data/__init__.py
@@ -1,6 +1,7 @@
 from formerbox.data.binarizer import Binarizer, DefaultBinarizer
 from formerbox.data.binarizer_seq2seq import Seq2SeqBinarizer
 from formerbox.data.dataset_converter import DatasetConverter, DefaultDatasetConverter
+from formerbox.data.dataset_iterators import DatasetIterator
 from formerbox.data.indexed_dataset import (
     IndexedCachedDataset,
     IndexedDataset,
