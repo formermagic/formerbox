ca9f8fe56657cd2e23813323651307baa120d34f
mozharovsky <mozharovsky@live.com>
2020-12-04 17:31:45 +0300

    refactor(tasks): get rid of useless generic types in TaskModule class

--- formerbox/tasks/task_module.py
+++ formerbox/tasks/task_module.py
@@ -1,31 +1,30 @@
 import logging
-from abc import abstractmethod
-from typing import Tuple, Type, TypeVar
+from abc import ABCMeta, abstractmethod
+from argparse import Namespace
+from typing import Tuple, Type, TypeVar, Union
 
+from formerbox.common.dataclass_argparse import DataclassBase, get_params_item
 from formerbox.common.has_params import HasParsableParams, ParamsType
 from formerbox.common.registrable import Registrable
+from pytorch_lightning import LightningDataModule, LightningModule
 from transformers import PreTrainedTokenizerFast as Tokenizer
-from typing_extensions import Protocol
 
-ModuleType = TypeVar("ModuleType")
-DataModuleType = TypeVar("DataModuleType")
+ModuleType = TypeVar("ModuleType", bound=LightningModule)
+DataModuleType = TypeVar("DataModuleType", bound=LightningDataModule)
 TaskModuleType = TypeVar("TaskModuleType", bound="TaskModule")
 
 logger = logging.getLogger(__name__)
 
 
-class TaskModuleBase(Protocol[ModuleType, DataModuleType]):
-    ...
-
-
 class TaskModule(
-    TaskModuleBase[ModuleType, DataModuleType],
     HasParsableParams[ParamsType],
     Registrable,
+    metaclass=ABCMeta,
 ):
     params: ParamsType
     params_type: Type[ParamsType]
 
+    @abstractmethod
     def __init__(
         self,
         tokenizer: Tokenizer,
@@ -40,6 +39,7 @@ class TaskModule(
     @classmethod
     @abstractmethod
     def setup(
-        cls: Type[TaskModuleType], params: Tuple[ParamsType, ...]
+        cls: Type[TaskModuleType],
+        params: Tuple[Union[DataclassBase, Namespace], ...],
     ) -> TaskModuleType:
         raise NotImplementedError()
