5e5b14c258bda9a2922f72c97548baabba11cfd1
mozharovsky <mozharovsky@live.com>
2020-12-26 22:25:34 +0300

    feat(data): add data collator for bart denoising unsupervised objective

--- formerbox/data/data_collator.py
+++ formerbox/data/data_collator.py
@@ -285,5 +285,209 @@ class DataCollatorForWholeWordMasking(DataCollatorForDenoising):
         return result_mask
 
 
-        """Returns random tokens sampled from the tokenizer vocab."""
-        return torch.randint(self.tokenizer.vocab_size, shape).long()
+@dataclass
+class DataCollatorForBartDenoising(DataCollatorForDenoising):
+    lambda_coef: float = 3.0
+
+    def __call__(self, features: List[Dict[Text, EncodedInput]]) -> Dict[Text, Tensor]:
+        assert self.tokenizer.pad_token_id is not None
+
+        inputs: List[EncodedInput] = []
+        for feature in features:
+            input_ids = feature["input_ids"]
+            inputs.append(input_ids)
+
+        input_ids, labels = self.add_noise(inputs)
+        attention_mask = (input_ids != self.tokenizer.pad_token_id).long()
+
+        return {
+            "input_ids": input_ids,
+            "labels": labels,
+            "attention_mask": attention_mask,
+        }
+
+    def add_noise(self, inputs: List[EncodedInput]) -> Tuple[Tensor, Tensor]:
+        # batched inputs for further collation
+        batched_input_ids: List[EncodedInput] = []
+        batched_labels: List[EncodedInput] = []
+
+        for input_ids in inputs:
+            # convert lists to tensors
+            if isinstance(input_ids, list):
+                input_ids = torch.tensor(input_ids)
+
+            # prepare masked input ids and labels
+            special_tokens_mask = self._get_special_tokens_mask(input_ids)
+            input_ids, labels = self.add_infiling_noise(input_ids, special_tokens_mask)
+
+            # append values to the accumulated lists
+            batched_input_ids.append(input_ids)
+            batched_labels.append(labels)
+
+        # collate accumulated tensors
+        input_ids = collate_batch(sequences=batched_input_ids, tokenizer=self.tokenizer)
+        labels = collate_batch(sequences=batched_labels, tokenizer=self.tokenizer)
+
+        return input_ids, labels
+
+    def add_insertion_noise(
+        self, tokens: Tensor, special_tokens_mask: Tensor, insertion_prob: float
+    ) -> Tuple[Tensor, Tensor]:
+        assert self.tokenizer.mask_token_id is not None
+
+        # count the number of insertions to make
+        tokens_mask = ~special_tokens_mask
+        num_tokens = tokens_mask.long().sum()
+        num_insertions = math.ceil(num_tokens * insertion_prob)
+
+        # select indices use for inserting mask tokens
+        insertions = torch.LongTensor(num_insertions).fill_(1)
+        insertions_prob = torch.cat((tokens_mask.long(), insertions), dim=0).float()
+        insertions_indices = torch.multinomial(
+            insertions_prob, num_samples=num_insertions, replacement=False
+        )
+
+        # a mask for tokens to be inserted
+        insertion_mask = torch.LongTensor(tokens.size(0) + num_insertions).fill_(0)
+        insertion_mask[insertions_indices] = 1
+        insertion_mask = insertion_mask.bool()
+
+        # a mask for tokens to be random among inserted indices
+        random_prob = torch.full(insertion_mask.shape, self.random_token_ratio)
+        random_mask = torch.bernoulli(random_prob).bool()
+        random_size = tokens[insertion_mask & random_mask].size()
+
+        # prepare the input tensor with insertions of mask and random tokens
+        input_ids = torch.LongTensor(tokens.size(0) + num_insertions).fill_(0)
+        input_ids[insertion_mask] = self.tokenizer.mask_token_id
+        input_ids[insertion_mask & random_mask] = self._random_tokens(random_size)  # type: ignore
+        input_ids[~insertion_mask] = tokens
+
+        # prepare the target tensor
+        labels = tokens.clone()
+
+        return input_ids, labels
+
+    def add_infiling_noise(
+        self, inputs: Tensor, special_tokens_mask: Tensor
+    ) -> Tuple[Tensor, Tensor]:
+        assert self.tokenizer.mask_token_id is not None
+
+        # count the number of maskable tokens
+        tokens_mask = ~special_tokens_mask
+        num_tokens = tokens_mask.long().sum().item()
+
+        # handle 0-tokens masking
+        if num_tokens == 0:
+            return inputs.clone(), inputs.clone()
+
+        # sample mask spans with the given masked token ratio
+        num_to_mask = math.ceil(float(num_tokens) * self.masked_token_ratio)
+        mask_spans = self._sample_mask_spans(num_to_mask)
+        # update the actual number of tokens to mask
+        num_inserts = num_to_mask - mask_spans.size(0)
+        num_to_mask = num_to_mask - num_inserts
+
+        # add insertion noise if 0-length span sampled
+        if num_to_mask == 0:
+            insertion_prob = num_inserts / num_tokens
+            return self.add_insertion_noise(
+                inputs,
+                insertion_prob=insertion_prob,
+                special_tokens_mask=special_tokens_mask,
+            )
+
+        # sample masking spans start indices
+        mask_indices = torch.multinomial(
+            tokens_mask.float(),
+            num_samples=num_to_mask,
+            replacement=False,
+        )
+
+        # mask to replace tokens with a mask token
+        replace_mask = torch.zeros_like(inputs)
+        # mask to keep original tokens in the output
+        original_mask = torch.ones_like(inputs)
+
+        for span_start, span_length in zip(mask_indices, mask_spans):
+            # num_tokens + 1 ensures that we mask at least one token
+            span_end = min(span_start + span_length, inputs.size(0))
+            span_length = span_end - span_start
+
+            # get span special tokens indices
+            span_tokens = special_tokens_mask[span_start:span_end]
+            span_indices = torch.nonzero(span_tokens, as_tuple=False)  # type: ignore
+            span_indices = span_indices[:, 0]
+
+            # keep tokens succeeding special tokens to split spaces into parts
+            # clamping prevents getting out of the span bounds
+            span_indices = (span_indices + 1).clamp_max(span_length - 1)
+
+            # mask for span tokens to keep in the result
+            span_keep_mask = torch.zeros_like(span_tokens)
+            # keep span special tokens in noised result
+            span_keep_mask[span_indices] = 1
+            # keep span start token as it gets masked
+            span_keep_mask[0] = 1
+
+            # prepare span mask for tokens to get replaced
+            replace_mask[span_start:span_end] = 1
+            # prepare span mask for tokens to keep in the result
+            original_mask[span_start:span_end] = span_keep_mask
+
+        # avoid replacing special tokens
+        replace_mask = replace_mask.bool() & tokens_mask
+        original_mask.masked_fill_(special_tokens_mask, value=1)
+        original_mask = original_mask.bool()
+
+        # prepare the input tensor with masked spans
+        input_ids = inputs.clone()
+        input_ids[replace_mask] = self.tokenizer.mask_token_id
+        input_ids = input_ids[original_mask]
+
+        # prepare the target tensor
+        labels = inputs.clone()
+
+        return input_ids, labels
+
+    def _sample_mask_spans(self, num_to_mask: int) -> Tensor:
+        """Returns the mask spans sampled from poisson PMF with given number of tokens to mask."""
+
+        # handle empty masking case
+        if num_to_mask == 0:
+            return torch.tensor([])
+
+        # sample masking spans from poisson prob mass function
+        lengths = self._sample_poisson(num_to_mask)
+
+        # make sure spans are long enough to take effect from masking
+        cum_lengths = lengths.cumsum(0)
+        while cum_lengths[-1] < num_to_mask:
+            span_lengths = self._sample_poisson(num_to_mask)
+            lengths = torch.cat((lengths, span_lengths), dim=0)
+            cum_lengths = lengths.cumsum(0)
+
+        # find upper bound span index for further trimming
+        last_span_index = 0
+        while cum_lengths[last_span_index] < num_to_mask:
+            last_span_index += 1
+
+        # trim upper bound span mask
+        if last_span_index != 0:
+            num_masked = cum_lengths[last_span_index - 1]
+            lengths[last_span_index] = num_to_mask - num_masked
+        else:
+            lengths[last_span_index] = num_to_mask
+
+        # update the number of spans to mask
+        num_to_mask = last_span_index + 1
+        # get spans subset of desired length
+        lengths = lengths[:num_to_mask]
+        # take only non-zero spans
+        lengths = lengths[lengths > 0]
+
+        return lengths
+
+    def _sample_poisson(self, length: int) -> Tensor:
+        """Returns a sample from poisson probability mass function of given length."""
+        return torch.poisson(torch.FloatTensor(length).fill_(self.lambda_coef)).long()
