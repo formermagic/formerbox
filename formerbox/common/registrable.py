aa6974077b5a30fdd1d5e16fcf409fd6af5f8478
mozharovsky <mozharovsky@live.com>
2021-01-16 03:42:01 +0300

    refactor(common): move generic-based registry to its own container class

--- formerbox/common/registrable.py
+++ formerbox/common/registrable.py
@@ -18,114 +18,170 @@ import importlib
 import logging
 import typing
 from collections import defaultdict
-from typing import Callable, Dict, List, Optional, Text, Tuple, Type, Union
+from typing import Callable, Dict, List, Optional, Text, Tuple, Type
 
 from formerbox.common.partial_initable import PartialInitable
 
-T = typing.TypeVar("T", bound="Registrable")  # pylint: disable=invalid-name
-
-RegistryKey = Union[Text, Type[T]]
-RegistryMap = Dict[Text, Tuple[Type[T], Optional[Text]]]
-Registry = Dict[RegistryKey[T], RegistryMap[T]]
+RegistryType = typing.TypeVar("RegistryType", bound="Registrable")
+RegistryWrapper = typing.TypeVar("RegistryWrapper", bound="Registrable")
+RegistryValue = Tuple[Type["Registrable"], Optional[Text]]
 
 logger = logging.getLogger(__name__)
 
 
+class Registry:
+    _registry: Dict[Text, RegistryValue]
+
+    def __init__(self) -> None:
+        self._registry = {}
+
+    def get(
+        self, key: Text, value_type: Type[RegistryType]
+    ) -> Tuple[Type[RegistryType], Optional[Text]]:
+        cls_candidate, constructor = self._get_candidate(key)
+        if issubclass(cls_candidate, value_type):
+            cls = typing.cast(Type[RegistryType], cls_candidate)
+            return cls, constructor
+        raise ValueError(f"Unable to get item of type {value_type}")
+
+    def keys(self, value_type: Type[typing.Any]) -> List[Text]:
+        result: List[Text] = []
+        for key, value in self._registry.items():
+            cls, _ = value
+            if issubclass(cls, value_type):
+                result.append(key)
+
+        return result
+
+    def __getitem__(self, key: Text) -> RegistryValue:
+        return self._get_candidate(key)
+
+    def __setitem__(self, key: Text, value: RegistryValue) -> None:
+        self._registry[key] = value
+
+    def __contains__(self, key: Text) -> bool:
+        return key in self._registry
+
+    def _get_candidate(self, key: Text) -> RegistryValue:
+        candidate: RegistryValue
+        if key in self._registry:
+            candidate = self._registry[key]
+        elif "." in key:
+            candidate = self._get_candidate_from_module(key)
+        else:
+            raise RuntimeError(
+                f"{key} is not a registered name. "
+                "You probably need to use the --include-package flag "
+                "to load your custom code. Alternatively, you can specify your choices "
+                'using fully-qualified paths, e.g. {"model": "my_module.models.MyModel"} '
+                "in which case they will be automatically imported correctly."
+            )
+
+        return candidate
+
+    def _get_candidate_from_module(self, key: Text) -> RegistryValue:
+        parts = key.split(".")
+        submodule = ".".join(parts[:-1])
+        class_name = parts[-1]
+
+        try:
+            module = importlib.import_module(submodule)
+        except ModuleNotFoundError as err:
+            raise RuntimeError(
+                f"tried to interpret {key} as a path to a class "
+                f"but unable to import module {submodule}"
+            ) from err
+
+        try:
+            subclass = getattr(module, class_name)
+            constructor = None
+        except AttributeError as err:
+            raise RuntimeError(
+                f"tried to interpret {key} as a path to a class "
+                f"but unable to find class {class_name} in {submodule}"
+            ) from err
+
+        return (subclass, constructor)
+
+
 class Registrable(PartialInitable):
-    default_implementation: Optional[Text] = None
-    _registry: Registry["Registrable"] = defaultdict(dict)
+    _registry: Dict[Text, Registry] = defaultdict(Registry)
+    _constructor: Optional[Text] = None
 
     @classmethod
     def register(
-        cls,
+        cls: Type["Registrable"],
         name: Text,
         constructor: Optional[Text] = None,
         exist_ok: bool = False,
-    ) -> Callable[[Type[T]], Type[T]]:
-        registry = Registrable._registry[cls]
+    ) -> Callable[[Type[RegistryWrapper]], Type[RegistryWrapper]]:
+        registry = Registrable._registry[cls.__name__]
 
-        def add_subclass_to_registry(subclass: Type[T]) -> Type[T]:
-            # Add to registry, raise an error if key has already been used.
-            if name in registry:
-                if not exist_ok:
-                    message = (
-                        f"{name} has already been registered as {registry[name][0].__name__}, but "
-                        f"exist_ok=True, so overwriting with {cls.__name__}"
-                    )
-                    raise RuntimeError(message)
+        def add_to_registry(
+            subclass: Type[RegistryWrapper],
+        ) -> Type[RegistryWrapper]:
+            if name in registry and not exist_ok:
+                message = f"{name} has already been registered as {registry[name][0].__name__}"
+                raise RuntimeError(message)
 
             registry[name] = (subclass, constructor)
             return subclass
 
-        return add_subclass_to_registry
+        return add_to_registry
 
     @classmethod
-    def resolve_class_name(cls: Type[T], name: Text) -> Tuple[Type[T], Optional[Text]]:
-        subclass: Type[T]
-        constructor: Optional[Text]
+    def resolve(
+        cls: Type[RegistryType], name: Text
+    ) -> Tuple[Type[RegistryType], Optional[Text]]:
+        registry = cls._get_registry()
+        return registry.get(name, value_type=cls)
 
-        if name in Registrable._registry[cls]:
-            subclass, constructor = Registrable._registry[cls][name]  # type: ignore
-        elif "." in name:
-            # This might be a fully qualified class name, so we'll try importing its "module"
-            # and finding it there.
-            parts = name.split(".")
-            submodule = ".".join(parts[:-1])
-            class_name = parts[-1]
-
-            try:
-                module = importlib.import_module(submodule)
-            except ModuleNotFoundError as err:
-                raise RuntimeError(
-                    f"tried to interpret {name} as a path to a class "
-                    f"but unable to import module {submodule}"
-                ) from err
-
-            try:
-                subclass = getattr(module, class_name)
-                constructor = None
-            except AttributeError as err:
-                raise RuntimeError(
-                    f"tried to interpret {name} as a path to a class "
-                    f"but unable to find class {class_name} in {submodule}"
-                ) from err
-
+    @classmethod
+    def from_registry(
+        cls: Type[RegistryType], name: Text
+    ) -> Tuple[Type[RegistryType], Callable[..., RegistryType]]:
+        registry = cls._get_registry()
+        subclass, constructor_name = registry.get(name, value_type=cls)
+        if constructor_name is None:
+            constructor = typing.cast(Callable, subclass)
         else:
-            # is not a qualified class name
-            raise RuntimeError(
-                f"{name} is not a registered name for {cls.__name__}. "
-                "You probably need to use the --include-package flag "
-                "to load your custom code. Alternatively, you can specify your choices "
-                """using fully-qualified paths, e.g. {"model": "my_module.models.MyModel"} """
-                "in which case they will be automatically imported correctly."
-            )
+            constructor = getattr(subclass, constructor_name)
+            constructor = typing.cast(Callable, constructor)
 
         return subclass, constructor
 
     @classmethod
-    def from_registry(cls: Type[T], name: Text) -> Tuple[Type[T], Callable[..., T]]:
-        subclass, constructor = cls.resolve_class_name(name)
-        if constructor is None:
-            return subclass, subclass.__new__  # type: ignore
-        return subclass, getattr(subclass, constructor)  # type: ignore
+    def from_name(cls: Type[RegistryType], name: Text) -> Callable[..., RegistryType]:
+        registry = cls._get_registry()
+        subclass, constructor_name = registry.get(name, value_type=cls)
+        if constructor_name is None:
+            constructor = typing.cast(Callable, subclass)
+        else:
+            constructor = getattr(subclass, constructor_name)
+            constructor = typing.cast(Callable, constructor)
 
-    @classmethod
-    def from_name(cls: Type[T], name: Text) -> Callable[..., T]:
-        subclass, constructor = cls.resolve_class_name(name)
-        if not constructor:
-            return subclass  # type: ignore
-        return getattr(subclass, constructor)
+        return constructor
 
     @classmethod
     def list_available(cls) -> List[Text]:
-        """List default first if it exists"""
-        keys = list(Registrable._registry[cls].keys())
-        default = cls.default_implementation
+        registry = cls._get_registry()
+        registered_names = registry.keys(cls)
+
         result: List[Text]
-        if default is None:
-            result = keys
-        elif default not in keys:
-            raise RuntimeError(f"Default implementation {default} is not registered")
+        if cls._constructor is None:
+            result = registered_names
+        elif cls._constructor not in registered_names:
+            raise RuntimeError(
+                f"Default implementation {cls._constructor} is not registered"
+            )
         else:
-            result = [default] + [k for k in keys if k != default]
+            result = [cls._constructor]
+            result += [name for name in registered_names if name != cls._constructor]
+
         return result
+
+    @classmethod
+    def _get_registry(cls: Type["Registrable"]) -> Registry:
+        if cls.__name__ not in Registrable._registry:
+            raise ValueError(f"{cls.__name__} is not a registered name")
+        return Registrable._registry[cls.__name__]
