f483d06880c250a3ff5d659aea5b8faa92dfa10f
mozharovsky <mozharovsky@live.com>
2021-01-17 23:28:55 +0300

    chore(tasks): export new task classes from tasks module

--- formerbox/tasks/__init__.py
+++ formerbox/tasks/__init__.py
@@ -3,6 +3,8 @@ import os
 from formerbox.tasks.denoising_task import DenoisingTask
 from formerbox.tasks.masked_lm_task import MaskedLMTask
 from formerbox.tasks.task_module import TaskModule
+from formerbox.tasks.translation_task import TranslationTask
+from formerbox.tasks.word_lm_task import WordLMTask
 from formerbox.utils.utils import str2bool
 
 EXPORT_TASKS = str2bool(os.environ.get("EXPORT_TASKS", default="true"))
